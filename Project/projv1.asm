2bdd50a70a754b1ac3d3ddcc2c8654b23f074bc2
sfrg-cell <jonyhoran@gmail.com>
2025-04-01 18:26:00 +0300

    added logic that opens a file

--- Project/projv1.asm
+++ Project/projv1.asm
@@ -1,26 +1,19 @@
 .model small
 .stack 100h
 .data 
-    oneChar db 0
-
+    filename db 'test.in', 0
+    buffer db 100 dup(0)
 .code
 main proc
 mov ax, SEG @data
 mov ds, ax           
 
-mov ah, 02h
-mov dl, '0'
+mov ah, 3Dh
+mov al, 00h 
+mov dx, offset filename
 int 21h
 
-read_next:
-    mov ah, 3Fh
-    mov bx, 0h  ; stdin handle
-    mov cx, 1   ; 1 byte to read
-    mov dx, offset oneChar   ; read to ds:dx 
-    int 21h   ;  ax = number of bytes read
-    ; do something with [oneChar]
-    or ax,ax
-    jnz read_next
+mov bx, ax 
 
 exit:
 mov ax, 4c00h
