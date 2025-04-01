32016443e831324f76eeb449a69da19d543425f3
sfrg-cell <jonyhoran@gmail.com>
2025-04-01 22:48:26 +0300

    count_substring proc implemented

--- Project/projv1.asm
+++ Project/projv1.asm
@@ -5,6 +5,9 @@
     buffer db 255 dup(0)
     newline db 13, 10, '$'
     line_count db 0 
+    key db 'aa', 0
+    match_count db 0
+    
 .code
 main proc
 mov ax, SEG @data
@@ -35,6 +38,8 @@ mov dx, offset newline
 mov ah, 09h
 int 21h
 
+call count_substring
+
 count_lines:
 inc line_count
 cmp line_count, 100
@@ -50,4 +55,44 @@ exit:
 mov ax, 4c00h
 int 21h
 main endp
+
+count_substring proc
+xor cx, cx
+lea si, buffer
+lea di, key
+
+compare_loop:
+mov al, [si]
+mov dl, [di]
+cmp al, dl
+jne match_not_found
+je match_found
+
+match_found:
+lea si, [si + 1]
+lea di, [di + 1]
+cmp si, 0
+je finish
+cmp di, 0
+je inc_count
+jmp compare_loop
+
+match_not_found:
+lea si, [si + 1]
+cmp si, 0
+je finish
+lea di, key
+jmp compare_loop
+
+inc_count:
+inc cx
+lea di, key
+jmp compare_loop
+
+finish:
+mov match_count, cl
+
+ret
+count_substring endp
+
 end main
