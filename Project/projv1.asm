47ba4650625b7d4449020deb3204d71af835a7bd
sfrg-cell <jonyhoran@gmail.com>
2025-04-02 23:26:17 +0300

    fixed logic that reads and opens file

--- Project/projv1.asm
+++ Project/projv1.asm
@@ -2,58 +2,114 @@
 .stack 100h
 .data 
     filename db 'test.in', 0
+    error_msg db 'Error opening file!$'
     buffer db 255 dup(0)
+    lines db 100*255 dup(0)  
     newline db 13, 10, '$'
-    line_count db 0 
+    line_count dw 0
+    char_count dw 0
+    line_pos dw 0
+    handle dw ?
     key db 'aa', 0
     match_count db 0
     
 .code
 main proc
-mov ax, SEG @data
-mov ds, ax           
+    mov ax, @data
+    mov ds, ax
 
 open_file:
-mov ah, 3Dh
-mov al, 00h 
-mov dx, offset filename
-int 21h
-mov bx, ax
+    mov ah, 3Dh
+    mov al, 00h
+    lea dx, filename
+    int 21h
+    jnc file_opened
+    mov ah, 09h
+    lea dx, error_msg
+    int 21h
+    jmp exit
+    
+file_opened:
+    mov handle, ax
+    mov line_count, 0
 
 read_file:
-mov ah, 3Fh
-mov cx, 255
-mov dx, offset buffer
-int 21h
+    mov ah, 3Fh
+    mov bx, handle
+    mov cx, 255
+    lea dx, buffer
+    int 21h
 
-cmp ax, 0
-je close_file
+    cmp ax, 0
+    je close_file
 
-mov dx, offset buffer
-mov ah, 09h
-int 21h
+    mov cx, ax
+    mov si, 0
 
-new_line:
-mov dx, offset newline
-mov ah, 09h
-int 21h
+process_char:
+    cmp si, cx
+    jae read_file
 
-call count_substring
+    mov al, buffer[si]
+    
+    cmp al, 0Dh
+    je found_cr
+    
+    cmp al, 0Ah
+    je found_lf
+    
+    mov di, line_count
+    mov ax, 255
+    mul di
+    mov di, ax
+    add di, line_pos
+    mov lines[di], al
+    inc line_pos
+    
+    inc si
+    jmp process_char
 
-count_lines:
-inc line_count
-cmp line_count, 100
-je close_file
+found_cr:
+    inc si
+    
+    cmp si, cx
+    jae save_line
+    
+    cmp buffer[si], 0Ah
+    jne save_line
+    
+    inc si
+    jmp save_line
 
-jmp read_file
+found_lf:
+    inc si
+
+save_line:
+    mov di, line_count
+    mov ax, 255
+    mul di
+    mov di, ax
+    add di, line_pos
+    mov lines[di], '$'
+
+    call count_substring
+    
+    inc line_count
+    mov line_pos, 0
+    
+    cmp line_count, 100
+    jge close_file
+    
+    jmp process_char
 
 close_file:
-mov ah, 3Eh      
-int 21h 
+    mov ah, 3Eh
+    mov bx, handle
+    int 21h
 
 exit:
-mov ax, 4c00h
-int 21h
+    mov ax, 4C00h
+    int 21h
 main endp
 
 count_substring proc
