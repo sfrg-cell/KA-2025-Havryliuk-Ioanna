851cb7cdde96c6ad40314f9e787cfc466f4021c7
sfrg-cell <jonyhoran@gmail.com>
2025-04-16 22:43:08 +0300

    Added comments, added hw vid

--- Labs/Lab09/iostr.asm
+++ Labs/Lab09/iostr.asm
@@ -14,42 +14,42 @@ main proc
     mov ax, @data
     mov ds, ax
     
-    mov cx, 20
-    mov si, 0
-    lea bx, strings
+    mov cx, 20                        ; Max number of lines to read
+    mov si, 0                         ; Index for pointers[]
+    lea bx, strings                   ; BX points to start of strings buffer
     
 input_loop:
     lea dx, prompt_msg
     mov ah, 09h
     int 21h
     
-    mov pointers[si], bx
+    mov pointers[si], bx              ; Store current string's start address
     
-    mov di, bx
+    mov di, bx                        ; DI points to current writing position
     
 read_line:
-    mov ah, 01h
+    mov ah, 01h                       ; Read character from keyboard
     int 21h
     
-    cmp al, 0Dh
+    cmp al, 0Dh                       ; Check for enter
     je end_of_line
     
-    mov [di], al
+    mov [di], al                      ; Store character in buffer
     inc di
     jmp read_line
     
 end_of_line:
     mov ax, di
-    sub ax, bx
+    sub ax, bx                        ; Count length of string
     
-    cmp ax, 0
+    cmp ax, 0                         ; Exit if string is empty
     je exit_input
     
-    mov byte ptr [di], '$'
+    mov byte ptr [di], '$'            ; Add string terminator
     inc di
     inc count
     mov bx, di
-    add si, 2
+    add si, 2                         ; Move to next slot in pointers[]
     loop input_loop
     jmp display_output
     
@@ -63,12 +63,12 @@ display_output:
     int 21h
     
     mov cx, count
-    jcxz exit_program
+    jcxz exit_program                 ; If no input, exit
     
     mov ax, count
     dec ax
     shl ax, 1
-    mov si, ax
+    mov si, ax                        ; SI points to last pointer
     
 output_loop:
     mov dx, pointers[si]
@@ -76,15 +76,15 @@ output_loop:
     int 21h
     
     cmp cx, 1
-    je skip_newline
+    je skip_newline                  ; Skip newline after last string
     
     lea dx, line_break
     mov ah, 09h
     int 21h
     
 skip_newline:
-    sub si, 2
-    loop output_loop
+    sub si, 2                         ; Move to previous string pointer
+    loop output_loop                  ; Repeat until all lines printed
     
 exit_program:
     mov ax, 4C00h
